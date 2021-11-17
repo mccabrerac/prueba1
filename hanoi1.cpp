6e958c3263c74ff70b39d361afead749e31154be
mccabrerac <miriam.cabrera0811@gmail.com>
2021-11-17 14:09:05 -0500

    HANOI con string

--- hanoi1.cpp
+++ hanoi1.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void hanoi(int , char, char, char);
+void hanoi(int , string, string, string);
 
 int main(){
     int n;
     cout<<"\nIngresar la cantidad de discos para el juego: "; cin>>n;
-    hanoi(n,'O', 'A', 'D');
+    hanoi(n,"origen", "aa", "destino");
     return 0;
 }
 
-void hanoi(int n, char o, char a, char d){
+void hanoi(int n, string origen, string aa, string destino){
     if(n==1){
-        cout<<"Mover el disco del "<<o<<" al "<<d<<endl;
+        cout<<"Mover el disco del "<<origen<<" al "<<destino<<endl;
     }
     else{
-        hanoi(n-1, o, d, a);
-        cout<<"Mover disco del "<<o<<" al " <<d<<endl;
-        hanoi(n-1,a,o,d);
+        hanoi(n-1, origen, destino, aa);
+        cout<<"Mover disco del "<<origen<<" al " <<destino<<endl;
+        hanoi(n-1,aa,origen,destino);
     }
 
 
